d7c1c53a57adf9e62ef16bfdcdd13c7686274689
Tomás Palma <up202108880@edu.fe.up.pt>
2024-03-23 11:09:27 +0000

    add: isImported checker on AnalysisUtils

--- src/main/pt/up/fe/comp2024/analysis/AnalysisUtils.java
+++ src/main/pt/up/fe/comp2024/analysis/AnalysisUtils.java
@@ -3,6 +3,7 @@ package pt.up.fe.comp2024.analysis;
 import pt.up.fe.comp.jmm.analysis.table.Symbol;
 import pt.up.fe.comp.jmm.analysis.table.SymbolTable;
 
+import java.util.List;
 import java.util.Optional;
 
 public class AnalysisUtils {
@@ -26,5 +27,12 @@ public class AnalysisUtils {
         }
 
         return Optional.empty();
+    }
+
+    public static boolean validateIsImported(String name, SymbolTable table) {
+        return table.getImports().stream().anyMatch(curr_import -> {
+            List<String> curr_import_params = List.of(curr_import.split("\\."));
+            return curr_import_params.get(curr_import_params.size() - 1).equals(name);
+        });
     }
 }
