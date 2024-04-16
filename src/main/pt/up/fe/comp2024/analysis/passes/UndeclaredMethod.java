97a0c49629e95554d6b8f12f631ae51b2c7d0799
Tomás Palma <up202108880@edu.fe.up.pt>
2024-04-16 23:03:58 +0100

    possible fix for ollir complex args

--- src/main/pt/up/fe/comp2024/analysis/passes/UndeclaredMethod.java
+++ src/main/pt/up/fe/comp2024/analysis/passes/UndeclaredMethod.java
@@ -10,6 +10,7 @@ import pt.up.fe.comp2024.analysis.AnalysisVisitor;
 import pt.up.fe.comp2024.ast.Kind;
 import pt.up.fe.comp2024.ast.NodeUtils;
 
+import java.util.List;
 import java.util.Optional;
 
 public class UndeclaredMethod extends AnalysisVisitor {
@@ -17,7 +18,13 @@ public class UndeclaredMethod extends AnalysisVisitor {
     @Override
     protected void buildVisitor() {
         addVisit(Kind.METHOD_DECL, this::visitMethodDecl);
+        addVisit(Kind.INNER_MAIN_METHOD, this::visitMainMethod);
         addVisit(Kind.VAR_METHOD, this::visitMethodCall);
+    }
+
+    private Void visitMainMethod(JmmNode method, SymbolTable table) {
+        this.currentMethod = "main";
+        return null;
     }
 
     private Void visitMethodDecl(JmmNode method, SymbolTable table) {
@@ -30,6 +37,29 @@ public class UndeclaredMethod extends AnalysisVisitor {
         methodCall.put("isDeclared", "False");
         JmmNode methodClass = methodCall.getChild(0);
 
+        if(methodClass.getKind().equals(Kind.PARENTHESIS.toString())) {
+            List<JmmNode> parenthesisChildren = methodClass.getChildren();
+
+            for(JmmNode child: parenthesisChildren) {
+                if(!child.getKind().equals(Kind.PARENTHESIS.toString())) {
+                    methodClass = child;
+                    break;
+                } else {
+                    JmmNode currChild = child;
+                    while(currChild.getKind().equals(Kind.PARENTHESIS.toString())) {
+                        if(currChild.getChildren().isEmpty()) break;
+                        currChild = currChild.getChild(0);
+                    }
+
+                    if(!currChild.getKind().equals(Kind.PARENTHESIS.toString())) {
+                        methodClass = currChild;
+                        break;
+                    }
+                }
+            }
+        }
+
+        // Isto está um esparguete enorme, mas não fui eu que fiz e não há tempo para resolver
         if(methodClass.getKind().equals("VarRefExpr")) {
             String varName = methodClass.get("name");
             Optional<Symbol> optSymbol = AnalysisUtils.validateSymbolFromSymbolTable(currentMethod, symbolTable, varName);
@@ -55,6 +85,31 @@ public class UndeclaredMethod extends AnalysisVisitor {
             if(isDeclared) methodCall.put("isDeclared", "True");
             if(isDeclared || !symbolTable.getSuper().isEmpty()) {
                 return null; // its either a function of this class or assumed is a function of super
+            }
+        } else if(methodClass.getKind().equals(Kind.NEW_CLASS.toString())) {
+            String callerClassName = methodClass.get("name");
+            boolean classNameIsMainClass = symbolTable.getClassName().equals(callerClassName);
+            if(classNameIsMainClass) {
+                boolean methodExistsInCallerClass = symbolTable.getMethods().stream().anyMatch((symbolTableRegisteredMethod) ->
+                    symbolTableRegisteredMethod.equals(methodName)
+                );
+
+                if(!methodExistsInCallerClass) {
+                    var message = String.format("Method %s is not declared", methodName);
+                    addReport(Report.newError(Stage.SEMANTIC, NodeUtils.getLine(methodCall),
+                            NodeUtils.getColumn(methodCall), message, null));
+                }
+
+                return null;
+            } else {
+                boolean isCallerClassImported = AnalysisUtils.validateIsImported(callerClassName, symbolTable);
+                if(!isCallerClassImported) {
+                    var message = String.format("Method %s is not declared", methodName);
+                    addReport(Report.newError(Stage.SEMANTIC, NodeUtils.getLine(methodCall),
+                            NodeUtils.getColumn(methodCall), message, null));
+                }
+
+                return null;
             }
         }
 
