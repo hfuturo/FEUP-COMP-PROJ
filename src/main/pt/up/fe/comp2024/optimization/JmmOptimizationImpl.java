3235630631b1d95d90ec4b858f89fba3abce7bf1
pfpo <pedro.oliveira2003g@gmail.com>
2024-05-14 11:33:32 +0100

    Temporary calling optimization visit on toOllir.

--- src/main/pt/up/fe/comp2024/optimization/JmmOptimizationImpl.java
+++ src/main/pt/up/fe/comp2024/optimization/JmmOptimizationImpl.java
@@ -3,6 +3,7 @@ package pt.up.fe.comp2024.optimization;
 import pt.up.fe.comp.jmm.analysis.JmmSemanticsResult;
 import pt.up.fe.comp.jmm.ollir.JmmOptimization;
 import pt.up.fe.comp.jmm.ollir.OllirResult;
+import pt.up.fe.comp2024.optimization_jasmin.ConstantPropagationOpt;
 
 import java.util.Collections;
 
@@ -10,6 +11,9 @@ public class JmmOptimizationImpl implements JmmOptimization {
 
     @Override
     public OllirResult toOllir(JmmSemanticsResult semanticsResult) {
+
+        var optVisitor = new ConstantPropagationOpt();
+        optVisitor.visit(semanticsResult.getRootNode(), semanticsResult.getSymbolTable());
 
         var visitor = new OllirGeneratorVisitor(semanticsResult.getSymbolTable());
         var ollirCode = visitor.visit(semanticsResult.getRootNode());
